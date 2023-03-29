0b0be17fa3860dc1c17289013bc0164730e1aed7
Juliia Kapuza <kapuzajuliia@gmail.com>
2023-03-29 15:47:02 +0300

    start work with vue

---
+++ vue-project/src/App.vue
@@ -0,0 +1,26 @@
+<script setup>
+import { reactive, ref } from "vue";
+
+const counter = reactive({ count: 20 });
+const msg = ref("Привіт, світе!");
+const titleClass = ref("title");
+const subtitleClass = ref("subtitle");
+const isButtonDisabled = true;
+</script>
+
+<template>
+  <div>
+    <h1 :class="titleClass">{{ msg }}</h1>
+    <p :class="subtitleClass">Рахунок: {{ counter.count }}</p>
+    <button :disabled="isButtonDisabled">Bind</button>
+  </div>
+</template>
+
+<style scoped>
+.title {
+  color: aqua;
+}
+.subtitle {
+  color: cadetblue;
+}
+</style>
