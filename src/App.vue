dc91e2bb6792f5ab09a4bfde7582f9c36faf90ce
Juliia Kapuza <kapuzajuliia@gmail.com>
2023-03-31 14:28:18 +0300

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<script setup>
+// import BasicStyle from './components/Basic.vue'
+// import ConditionalRendering from './components/ConditionalRending.vue'
+import EventListening from './components/EventListening.vue'
+// import FormEvent from './components/FormEvent.vue';
+</script>
+
+<template>
+  <div>
+    
+    <!-- <BasicStyle/> -->
+    <!-- <ConditionalRendering/> -->
+    <EventListening/>
+    <!-- <FormEvent/> -->
+
+  </div>
+</template>
+
+<style scoped>
+
+</style>
---
+++ src/components/FormEvent.vue
@@ -0,0 +1,36 @@
+<script setup>
+import { ref } from "vue";
+
+// const header = ref("Shoping List App");
+const items = [
+  { id: 1, label: "10 party hats" },
+  { id: 2, label: "2 board games" },
+  { id: 3, label: "20 cupst" },
+];
+
+const newItem = ref("");
+</script>
+
+<template>
+  <div>
+    <!-- <h1>{{ header }}</h1> -->
+    <form
+      v-on:submit.prevent="items.push({ id: items.length + 1, label: newItem })"
+    >
+      <input v-model.trim="newItem" type="text" placeholder="Add an item" />
+      <button>Save Items</button>
+    </form>
+
+    <ul>
+      <li class="list" v-for="{ id, label } in items" :key="id">
+        {{ label }}
+      </li>
+    </ul>
+  </div>
+</template>
+
+<style scoped>
+.list {
+  color: cadetblue;
+}
+</style>
